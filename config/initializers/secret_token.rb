d577c4cf482a3f81567cd0cd8f7ca9e66cd78d03
jameskyokushin <james@kyokushin.com.ph>
2011-09-21 15:24:42 +0800

    initial commit using activeadmin gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServiceCall::Application.config.secret_token = '7108e047850c08f4d651ffb353f59ac9a4461d98a9d1ab57308257290b37ca81054f98f18f08d306bab6d8cb5bc23747fd78e723c414004b6198c82334e1d4a1'
